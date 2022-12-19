e6b70a636a2eeb896874d302a543c8df5220904c
Amruth sagar <amruthsagar1024@outlook.com>
2022-12-20 00:33:08 +0530

    Created overfit_test.py, and changes to train.py, and a new function in dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,20 +1,56 @@
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SubsetRandomSampler
 import torch
+import random
 
 
 def collate_function(batch):
     inputs = torch.stack([x[0] for x in batch])
     labels = torch.tensor([x[1] for x in batch])
 
-    return (inputs, labels)         # (batch_size, channels, h, w),  (batch_size)
+    # (batch_size, channels, h, w),  (batch_size)
+    return (inputs, labels)
+
+
+def make_train_val_loaders(dataset, split, train_bs, valid_bs):
+    """
+        This function returns train_dataloader and validation_dataloader 
+        based on the split specified.
+
+        dataset: dataset object
+        split: A tuple, or a list with float values summing to 1
+        train_bs: batch size of train dataloader
+        valid_bs: batch size of validation dataloader
+    """
+    assert isinstance(split, tuple) or isinstance(split, list)
+    assert sum(split) == 1
+
+    indices = list(range(len(dataset)))
+    random.seed(42)
+    random.shuffle(indices)
+
+    split_index = int(split[0]*len(dataset))
+    train_subsampler = SubsetRandomSampler(indices[0:split_index])
+    valid_subsampler = SubsetRandomSampler(indices[split_index:])
+
+    return (make_data_loader(dataset=dataset,
+                             batch_size=train_bs,
+                             num_workers=4,
+                             sampler=train_subsampler),
+            make_data_loader(dataset=dataset,
+                             batch_size=valid_bs,
+                             num_workers=2,
+                             sampler=valid_subsampler)
+            )
 
 
 def make_data_loader(dataset,
                      batch_size,
-                     num_workers):
+                     num_workers,
+                     sampler=None):
 
     return DataLoader(dataset,
                       batch_size=batch_size,
                       collate_fn=collate_function,
                       num_workers=num_workers,
-                      persistent_workers=True)
+                      persistent_workers=True,
+                      sampler=sampler)
--- overfit_test.py
+++ overfit_test.py
@@ -24,12 +24,11 @@ dataset = AdsNonAds(images_dir=config['data']['data_dir'],
                     img_width=config['data']['reshape_width'],
                     seed=42,
                     of_num_imgs=config['of_test']['num_imgs_per_cls'],
-                    overfit_test=False)
+                    overfit_test=config['of_test']['do_overfit_test'])
 
-train_dataloader, valid_dataloader = make_train_val_loaders(dataset=dataset,
-                                                            split=(0.95, 0.05),
-                                                            train_bs=config['model']['train_bs'],
-                                                            valid_bs=config['model']['valid_bs'])
+dataloader = make_data_loader(dataset=dataset,
+                              batch_size=config['of_test']['batch_size'] if config['of_test']['do_overfit_test'] else config['model']['batch_size'] ,
+                              num_workers=2)
 
 
 pretrained_model = ViTModel.from_pretrained(config['model']['vit_pretrained'])
@@ -45,7 +44,7 @@ optimizer = optim.SGD(vit_model.parameters(),
                       lr=config['model']['learning_rate'], momentum=0.9)
 
 
-previous_valid_loss = 1e10
+previous_epoch_loss = 1e10
 
 epochs = config['of_test']['epochs'] if config['of_test']['do_overfit_test'] else config['model']['epochs']
 
@@ -53,14 +52,16 @@ epochs = config['of_test']['epochs'] if config['of_test']['do_overfit_test'] els
 vit_model.train()
 
 for epoch in range(epochs):
-    train_loss = []
-    valid_loss = []
-
+    epoch_loss = []
     print("EPOCH:{0}".format(epoch))
 
-    for batch in tqdm(train_dataloader):
+    for batch in tqdm(dataloader):
         inputs, labels = batch
         inputs, labels = inputs.to(device), labels.to(device)
+
+        # If we want only the features from the VitModel, without training it.
+        # with torch.no_grad():
+        #     hidden_state = vit_model(inputs).last_hidden_state
 
         optimizer.zero_grad()
 
@@ -69,41 +70,31 @@ for epoch in range(epochs):
         loss.backward()
 
         optimizer.step()
-        train_loss.append(loss.item())
+        epoch_loss.append(loss.item())
 
-    train_loss = np.mean(train_loss)
+    epoch_loss = np.mean(epoch_loss)
 
-    with torch.no_grad():
-        for batch in tqdm(valid_dataloader):
-            inputs, labels = batch
-            inputs, labels = inputs.to(device), labels.to(device)
-            
-            output = vit_model(inputs)
-            loss = criterion(output, labels)
-
-            valid_loss.append(loss.item())
-
-    valid_loss = np.mean(valid_loss)
-    
     if wandb_log:
-        wandb.log({'loss/train': train_loss, 'loss/val' : valid_loss})
+        wandb.log({'train_loss': epoch_loss})
 
-    if epoch % config['ckpts']['ckpt_frequency'] == 0:
-        save_states = {
-            'epoch': epoch,
-            'model_state_dict': cls_head.state_dict(),
-            'optimizer': optimizer.state_dict()
-        }
-        save_checkpoint(state=save_states, is_best=False,
-                        file_folder=config['ckpt']['ckpt_folder'],
-                        file_name='epoch_{:03d}.pth.tar'.format(epoch)
-                        )
-        if valid_loss < previous_valid_loss:
-            previous_valid_loss = valid_loss
-            save_checkpoint(state=save_states,
-                            is_best=True,
+    if not config['of_test']['do_overfit_test']:
+        if epoch % config['ckpts']['ckpt_frequency'] == 0:
+            save_states = {
+                'epoch': epoch,
+                'model_state_dict': cls_head.state_dict(),
+                'optimizer': optimizer.state_dict()
+            }
+            save_checkpoint(state=save_states, is_best=False,
                             file_folder=config['ckpt']['ckpt_folder'],
                             file_name='epoch_{:03d}.pth.tar'.format(epoch)
                             )
+            if epoch_loss < previous_epoch_loss:
+                previous_epoch_loss = epoch_loss
+                save_checkpoint(state=save_states,
+                                is_best=True,
+                                file_folder=config['ckpt']['ckpt_folder'], 
+                                file_name='epoch_{:03d}.pth.tar'.format(epoch)
+                                )
+
 if wandb_log:
     wandb.finish()
