605f65371b8254709caf182e54d3c4e570e24939
Amruth sagar <amruthsagar1024@outlook.com>
2022-12-20 15:23:13 +0530

    Changed the interpolation mode in image_transform

--- datasets.py
+++ datasets.py
@@ -1,15 +1,18 @@
 from glob import glob
-from torch.utils.data import  Dataset
+from torch.utils.data import Dataset
 import torchvision.transforms as T
 import os
 import random
-from matplotlib.pyplot import imread
+import cv2
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class AdsNonAds(Dataset):
     def __init__(self,
                  images_dir,
-                 img_height=224, 
+                 img_height=224,
                  img_width=224,
                  seed=42,
                  of_num_imgs=20,
@@ -25,10 +28,12 @@ class AdsNonAds(Dataset):
 
         self.image_transforms = T.Compose([
             T.ToPILImage(),
-            T.Resize((img_height, img_width), T.InterpolationMode.BICUBIC),
             T.ToTensor(),
-            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
+            lambda x: x.unsqueeze(0),           # Because F.interpolate expects in N, channels, h, w
+            lambda x: F.interpolate(x, size=(img_height, img_width), mode='area'),
+            lambda x: x.squeeze(0),             # Squeeze back the dimension 0
+            T.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])    # Mean and std of ImageNet
+            ]) 
 
     def train_dataset(self, seed):
         ads = glob(self.data_dir+'/ads/*.jpeg')
@@ -64,6 +69,12 @@ class AdsNonAds(Dataset):
 
     def __getitem__(self, idx):
         filename = self.dataset[idx][0]
-        image = imread(filename)
-
+        
+        # The shape of the image is (height, width, channels)
+        # any image is read as BGR image --> converted to RGB
+        image = cv2.cvtColor(cv2.imread(
+            filename,
+            flags=cv2.IMREAD_COLOR),
+            code=cv2.COLOR_BGR2RGB)
+        
         return (self.image_transforms(image), self.dataset[idx][1])
