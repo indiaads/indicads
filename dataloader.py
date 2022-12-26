1832621a553b49ab4d59b7a9fbe1364e50c7bcca
Amruth sagar <amruthsagar1024@outlook.com>
2022-12-26 21:59:38 +0530

    Removed make_train_val_dataloader function. AUGMENT feature is added, so that if a dataset has augmentation, and it send a list of images after augmentation, the collate function behaves differently in that case

--- dataloader.py
+++ dataloader.py
@@ -3,54 +3,36 @@ import torch
 import random
 
 
-def collate_function(batch):
-    inputs = torch.stack([x[0] for x in batch])
-    labels = torch.tensor([x[1] for x in batch])
+def collate_function(batch, augment=False):
+    if augment:
+        inputs = []
+        labels = []
+        for img_list, label_list  in batch:
+            inputs.extend(img_list)
+            labels.extend(label_list)
+        zipped = list(zip(inputs, labels))
+        random.shuffle(zipped)
+        inputs, labels = zip(*zipped)
+
+        inputs = torch.stack(inputs)
+        labels = torch.tensor(labels)
+    else:
+        inputs = torch.stack([x[0] for x in batch])
+        labels = torch.tensor([x[1] for x in batch])
 
     # (batch_size, channels, h, w),  (batch_size)
     return (inputs, labels)
 
 
-def make_train_val_loaders(dataset, split, train_bs, valid_bs):
-    """
-        This function returns train_dataloader and validation_dataloader 
-        based on the split specified.
-
-        dataset: dataset object
-        split: A tuple, or a list with float values summing to 1
-        train_bs: batch size of train dataloader
-        valid_bs: batch size of validation dataloader
-    """
-    assert isinstance(split, tuple) or isinstance(split, list)
-    assert sum(split) == 1
-
-    indices = list(range(len(dataset)))
-    random.seed(42)
-    random.shuffle(indices)
-
-    split_index = int(split[0]*len(dataset))
-    train_subsampler = SubsetRandomSampler(indices[0:split_index])
-    valid_subsampler = SubsetRandomSampler(indices[split_index:])
-
-    return (make_data_loader(dataset=dataset,
-                             batch_size=train_bs,
-                             num_workers=4,
-                             sampler=train_subsampler),
-            make_data_loader(dataset=dataset,
-                             batch_size=valid_bs,
-                             num_workers=2,
-                             sampler=valid_subsampler)
-            )
-
-
 def make_data_loader(dataset,
                      batch_size,
                      num_workers,
-                     sampler=None):
+                     sampler=None,
+                     data_augment=False):
 
     return DataLoader(dataset,
                       batch_size=batch_size,
-                      collate_fn=collate_function,
+                      collate_fn=lambda x: collate_function(x, data_augment),
                       num_workers=num_workers,
                       persistent_workers=True,
                       sampler=sampler)
