9ee7abf7962a5fdf4be08980f51573529f856585
Amruth sagar <amruthsagar1024@outlook.com>
2022-12-29 09:49:41 +0530

    now, best checkpoint shows the epoch number

--- utils.py
+++ utils.py
@@ -10,10 +10,12 @@ def save_checkpoint(state, is_best, file_folder, experiment,
     os.makedirs(file_folder, exist_ok=True)
     torch.save(state, os.path.join(file_folder, file_name))
     if is_best:
+        
         # skip the optimization / scheduler state
-        state.pop('optimizer', None)
-        state.pop('scheduler', None)
-        torch.save(state, os.path.join(file_folder, 'model_best.pth.tar'))
+        # If key not found, returns 'None' instead of raising errors
+        state.pop('optimizers', None)
+        state.pop('schedulers', None)
+        torch.save(state, os.path.join(file_folder, file_name[:-8]+'_best.pth.tar'))
 
 
 def load_config(config_file):
