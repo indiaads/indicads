2bfb1edb71ab69aef5a85eca0059c4e3836e5f6a
Amruth sagar <amruthsagar1024@outlook.com>
2022-12-29 09:53:54 +0530

    Replace the custom image transformation with ViTFeatureExtractor

--- datasets.py
+++ datasets.py
@@ -4,9 +4,8 @@ import torchvision.transforms as T
 import os
 import random
 import cv2
-import torch.nn.functional as F 
 import torchvision.transforms as T
-import torch
+from transformers import ViTFeatureExtractor
 
 class AdsNonAds(Dataset):
     def __init__(self,
@@ -27,14 +26,7 @@ class AdsNonAds(Dataset):
         else:
             self.dataset = self.train_dataset(seed)
 
-        self.image_transforms = T.Compose([
-            T.ToPILImage(),
-            T.ToTensor(),
-            lambda x: x.unsqueeze(0),           # Because F.interpolate expects in N, channels, h, w
-            lambda x: F.interpolate(x, size=(img_height, img_width), mode='area'),
-            lambda x: x.squeeze(0),             # Squeeze back the dimension 0
-            T.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])    # Mean and std of ImageNet
-            ]) 
+        self.image_transforms = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224-in21k')
 
     def train_dataset(self, seed):
         ads = glob(self.data_dir+'/ads/*')
@@ -77,9 +69,8 @@ class AdsNonAds(Dataset):
             filename,
             flags=cv2.IMREAD_COLOR),
             code=cv2.COLOR_BGR2RGB)
+        transformed_image = self.image_transforms(image, return_tensors="pt")["pixel_values"].squeeze(0) # 3, H, W
         if self.augment:
-            transformed_image = self.image_transforms(image) # 3, H, W
-
             # All the transformation expect the image to be in shape of [...., H, W]
             rotated_90 = T.functional.rotate(transformed_image, angle=90)
             rotated_270 = T.functional.rotate(transformed_image, angle=270) 
@@ -88,4 +79,4 @@ class AdsNonAds(Dataset):
             return_list = [transformed_image, rotated_90, rotated_270, flipped]
 
             return (return_list, [self.dataset[idx][1]]*len(return_list))
-        return (self.image_transforms(image), self.dataset[idx][1])
+        return (transformed_image, self.dataset[idx][1])
